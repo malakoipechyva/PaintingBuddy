84820f87d8ed4dc4b621d04d65ae72b7a833f157
Yauhen  Chyrko <chirko.eugene@gmail.com>
2021-04-23 14:01:38 +0300

    change custom image view layout

--- PaintingBuddy/View/PaintingView.swift
+++ PaintingBuddy/View/PaintingView.swift
@@ -11,19 +11,27 @@ class PaintingView: UIScrollView {
     
     let imageView: UIImageView = {
        let iview = UIImageView()
+        iview.contentMode = .scaleAspectFill
         return iview
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
+        backgroundColor = .black
+        bounces = false
 
         addSubview(imageView)
-        imageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
-
+        imageView.centerY(inView: self)
+        imageView.anchor(left: leftAnchor, right: rightAnchor)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PaintingView: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
